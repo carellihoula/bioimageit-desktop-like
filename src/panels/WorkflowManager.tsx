264a4a15b31f5e8e8b55a93299c8c1d53a5ac0d4
carellihoula <carellihoula10@gmail.com>
2025-05-07 12:35:52 +0200

    fix:use a single return for loading, error and data status.

--- src/panels/WorkflowManager.tsx
+++ src/panels/WorkflowManager.tsx
@@ -1,15 +1,13 @@
 import { useExportFlow } from "@/hooks/workflow-ui/useExportFlow";
-import { Button, VStack, Box, Text } from "@chakra-ui/react";
+import { Button, VStack, Box, Text, Spinner } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
 import { useState } from "react";
 
-const workflows = [
-  "/home/carellihoula/Documents/demoworkflow/demoworkflow",
-  "/home/carellihoula/Documents/testB",
-  "/home/carellihoula/Documents/workflows/FISH2",
-  "/home/carellihoula/Documents/workflows/testB",
-  "/home/carellihoula/Documents/bioimageit/demobio",
-  "/home/carellihoula/Documents/testA",
-];
+async function fetchWorkflows(): Promise<string[]> {
+  const response = await fetch("http://localhost:8000/api/workflows");
+  const data: string[] = await response.json();
+  return data;
+}
 
 /**
  *
@@ -18,6 +16,11 @@ const workflows = [
  */
 export const WorkflowManager = () => {
   const [selected, setSelected] = useState<string | null>(null);
+  const { isPending, error, data } = useQuery({
+    queryKey: ["getWorkflows"],
+    queryFn: fetchWorkflows,
+    // staleTime: 5 * 60 * 1000, // 5 minutes
+  });
   const exportFlow = useExportFlow("workflow.json");
 
   return (
@@ -64,21 +67,49 @@ export const WorkflowManager = () => {
         bg="dvBackground"
         borderRadius="md"
       >
-        {workflows.map((path) => (
+        {isPending && (
           <Text
-            key={path}
-            color={"dv-fg"}
-            px={2}
-            py={1}
-            borderRadius="sm"
-            cursor="pointer"
-            bg={selected === path ? "dvHoverBg" : "transparent"}
-            _hover={{ bg: "dvHoverBg" }}
-            onClick={() => setSelected(path)}
+            color="dv-fg"
+            className="flex items-center justify-center h-full"
           >
-            {path}
+            <Spinner size="lg" />
           </Text>
-        ))}
+        )}
+        {isPending ? (
+          <Text
+            color="dv-fg"
+            className="flex items-center justify-center h-full"
+          >
+            <Spinner size="lg" />
+          </Text>
+        ) : error ? (
+          <Text
+            color="dv-fg"
+            className="flex items-center justify-center h-full text-center"
+          >
+            An error has occurred: {error.message}
+          </Text>
+        ) : data && data.length === 0 ? (
+          <Text color="dv-fg" textAlign="center" py={2}>
+            No workflows found.
+          </Text>
+        ) : (
+          data?.map((path) => (
+            <Text
+              key={path}
+              color="dv-fg"
+              px={2}
+              py={1}
+              borderRadius="sm"
+              cursor="pointer"
+              bg={selected === path ? "dvHoverBg" : "transparent"}
+              _hover={{ bg: "dvHoverBg" }}
+              onClick={() => setSelected(path)}
+            >
+              {path}
+            </Text>
+          ))
+        )}
       </Box>
 
       <Button
