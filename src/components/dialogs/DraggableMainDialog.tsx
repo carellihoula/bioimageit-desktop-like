0fdb7aee81f8e3848c70f16658b67550bba4603d
carellihoula <carellihoula10@gmail.com>
2025-06-24 16:58:27 +0200

    feat: add DialogTypes constant for workflow dialog titles and update DraggableMainDialog for dynamic title rendering

--- src/components/dialogs/DraggableMainDialog.tsx
+++ src/components/dialogs/DraggableMainDialog.tsx
@@ -1,5 +1,8 @@
+import { DialogTypes } from "@/lib/const";
+import { useDialogStore } from "@/store/useDialogStore";
 import { Dialog, Portal } from "@chakra-ui/react";
-import { VscChromeMaximize, VscChromeClose } from "react-icons/vsc";
+import { VscChromeClose, VscChromeMinimize } from "react-icons/vsc";
+
 // import { VscChromeRestore } from "react-icons/vsc";
 
 interface DraggableDialogProps {
@@ -13,6 +16,8 @@ export const DraggableMainDialog = ({
   isOpen,
   onClose,
 }: DraggableDialogProps) => {
+  const type = useDialogStore((state) => state.type);
+
   return (
     <Dialog.Root
       closeOnInteractOutside={false}
@@ -29,39 +34,43 @@ export const DraggableMainDialog = ({
         <Dialog.Positioner>
           <Dialog.Content>
             <div
-              className=" flex items-center w-full justify-end draggable-dialog-header dv-height-tab dv-bg-tab-container  pr-2 custom-window-controls"
+              className="flex items-center w-full justify-between dv-height-tab dv-bg-tab-container pr-2"
               style={{
                 cursor: "move",
-                flexShrink: 0, // Prevents the header from shrinking
+                flexShrink: 0,
               }}
             >
-              <button
-                // onClick={handleCloseWindow}
-                className="p-1 hover:bg-red-500 hover:text-white dark:hover:bg-red-600 rounded"
-                aria-label="Close Window"
-                title="Close Window"
-              >
-                <VscChromeMaximize onClick={onClose} />
-              </button>
-              <button
-                // onClick={handleCloseWindow}
-                className="p-1 hover:bg-red-500 hover:text-white dark:hover:bg-red-600 rounded"
-                aria-label="Close Window"
-                title="Close Window"
-              >
-                <VscChromeClose onClick={onClose} />
-              </button>
+              {/* Invisible spacer for balance */}
+              <div className="flex-1"></div>
+
+              {/* Centered title */}
+              {DialogTypes.map(
+                (typeItem, index) =>
+                  type === typeItem.type && (
+                    <h1 key={index} className="flex-1 text-center dv-fg">
+                      {typeItem.title}
+                    </h1>
+                  )
+              )}
+              {/* Buttons on the right */}
+              <div className="flex-1 flex justify-end gap-1">
+                <button
+                  className="p-1 dv-fg dv-icon-hover hover:bg-red-500 hover:text-white dark:hover:bg-red-600 rounded"
+                  aria-label="Maximize Window"
+                  title="Maximize Window"
+                >
+                  <VscChromeMinimize onClick={onClose} />
+                </button>
+                <button
+                  className="p-1 dv-fg hover:bg-red-500 rounded"
+                  aria-label="Close Window"
+                  title="Close Window"
+                >
+                  <VscChromeClose onClick={onClose} />
+                </button>
+              </div>
             </div>
             <Dialog.Body>{children}</Dialog.Body>
-            {/* <Dialog.Footer>
-                <Dialog.ActionTrigger asChild>
-                  <Button variant="outline">Cancel</Button>
-                </Dialog.ActionTrigger>
-                <Button>Confirm</Button>
-              </Dialog.Footer> */}
-            {/* <Dialog.CloseTrigger asChild>
-                <CloseButton size="sm" />
-              </Dialog.CloseTrigger> */}
           </Dialog.Content>
         </Dialog.Positioner>
         ;
