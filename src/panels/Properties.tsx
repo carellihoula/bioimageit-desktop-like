35f27d126722696a7330f771ba725ef9feff2c9d
carellihoula <carellihoula10@gmail.com>
2025-08-12 16:00:15 +0200

    Moved useIsSourceNode into utils.ts

--- src/panels/Properties.tsx
+++ src/panels/Properties.tsx
@@ -11,16 +11,8 @@ import { Input } from "@chakra-ui/react";
 import { useReactFlow, useStore } from "@xyflow/react";
 import { CustomSelectForProperties } from "@/components/Properties/CustomSelectForProperties";
 import { useState } from "react";
+import { useIsSourceNode } from "@/utils/utils";
 
-function useIsSourceNode(nodeId?: string) {
-  return useStore((state) => {
-    if (!nodeId) return false;
-
-    // Check if this node is used as a target in an edge
-    const hasIncoming = state.edges.some((edge) => edge.target === nodeId);
-    return !hasIncoming; // true if it is a source node
-  });
-}
 /*
  * @param node - The node metadata containing inputs, outputs and description
  * @returns Array of accordion section objects
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,16 +1,11 @@
-import React from "react";
-import { fileIconMapping } from "../components/icons/FileIconMapping";
+import { useStore } from "@xyflow/react";
 
-interface FileIconProps {
-  filename: string;
+export function useIsSourceNode(nodeId?: string) {
+  return useStore((state) => {
+    if (!nodeId) return false;
+
+    // Check if this node is used as a target in an edge
+    const hasIncoming = state.edges.some((edge) => edge.target === nodeId);
+    return !hasIncoming; // true if it is a source node
+  });
 }
-
-const FileIcon: React.FC<FileIconProps> = ({ filename }) => {
-  // On extrait l'extension en prenant le texte après le dernier point.
-  const extMatch = filename.match(/\.([^.]+)$/);
-  const ext = extMatch ? extMatch[1].toLowerCase() : "";
-  // Choix de l'icône : si l'extension n'est pas présente dans le mapping, on renvoie l'icône par défaut.
-  return fileIconMapping[ext] ? fileIconMapping[ext] : fileIconMapping.default;
-};
-
-export default FileIcon;
